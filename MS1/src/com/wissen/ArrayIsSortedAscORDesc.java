b6a473c4463012f9d59cfd54659983bd87eb4452
dhakadrohan98 <rohandhakad20@gmail.com>
2024-06-30 17:00:44 +0530

    Maximum product subarray problem, TC: O(n) & SC: O(1)

---
+++ MS1/src/com/wissen/ArrayIsSortedAscORDesc.java
@@ -0,0 +1,59 @@
+package com.wissen;
+
+public class ArrayIsSortedAscORDesc {
+	
+	//TC: O(n)
+	//SC: O(1)
+	public static boolean isArraySorted(int[] nums) {
+		int n = nums.length;
+
+		// edge case
+		if (n <= 1)
+			return true; // An array with 0 or 1 element is always sorted
+
+		int i = 0;
+		// if numbers are duplicate then take i pointer to the index where element at
+		// index & index+1 are different
+		while (i + 1 < n && nums[i] == nums[i + 1]) {
+			i++;
+		}
+		int first = nums[i];
+		int second = nums[i + 1];
+		boolean isAscending = false;
+
+		if (first < second) {
+			isAscending = true;
+		}
+		if (isAscending) {
+			int j = i + 1;
+			while (j < n) {
+				if (nums[j] < nums[i]) {
+					return false;
+				}
+				i++;
+				j++;
+			}
+		} else {
+			int j = i + 1;
+			while (j < n) {
+				if (nums[j] > nums[i]) {
+					return false;
+				}
+				i++;
+				j++;
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		int[] nums1 = { 1, 1, 2, 3, 3, 4, 4, 4, 5 };
+		int[] nums2 = { 10, 10, 10, 9, 8, 8, 9, 6, 4, 4, 1 };
+
+		System.out.println(isArraySorted(nums1));
+		System.out.println(isArraySorted(nums2));
+
+	}
+
+}
--- MS1/src/com/wissen/blind75/MaximumProductSubarray.java
+++ MS1/src/com/wissen/blind75/MaximumProductSubarray.java
@@ -4,26 +4,29 @@ public class MaximumProductSubarray {
 
 	//TC: O(n)
     //SC: O(n)
-    public int maxProduct(int[] nums) {
+	public int maxProduct(int[] nums) {
         int n = nums.length;
-        int maxProd = Integer.MIN_VALUE;
-        int prefix = 1;
-        int suffix = 1;
-        
-        for(int i=0,j=n-1; i<n && j>=0; i++,j--) {
-            if(prefix == 0) {
-               prefix = 1;
-            }
-            if(suffix == 0) {
-               suffix = 1;
-            }
-            
-            prefix *= nums[i];
-            suffix *= nums[j-1];
-            
-            maxProd = Math.max(maxProd, Math.max(suffix, prefix));
+        if(n == 0) {
+            return 0;
         }
-        return maxProd;
+        int ans = nums[0];
+        int maxProd = nums[0];
+        int minProd = nums[0];
+        
+        for(int i=1; i<n; i++) {
+        //if current element is negative then swap maxProd with minProd
+            //because minProd * -ve = +ve
+            if(nums[i] < 0) {
+                int temp = maxProd;
+                maxProd = minProd;
+                minProd = temp;
+            }
+            
+            maxProd = Math.max(nums[i], nums[i] * maxProd);
+            minProd = Math.min(nums[i], nums[i] * minProd);
+            ans = Math.max(ans, maxProd);
+        }
+        return ans;
     }
     
 	public static void main(String[] args) {
