94980acd5a3b4ac60dab27421350b1a4968bcd62
dhakadrohan98 <rohandhakad20@gmail.com>
2024-07-25 12:07:39 +0530

    Flood fill, No. of provinces and No. of islands problems on graph are solved

---
+++ DSA_Revision/src/com/wissen/graph/NoOfProvinces.java
@@ -0,0 +1,30 @@
+package com.wissen.graph;
+
+public class NoOfProvinces {
+	 //TC: O(V+E)
+    //SC: O(V)
+    //Count total no. of components of a graph
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = 0;
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        for(int sr=0; sr<n; sr++) {
+            //first node of new component of a graph
+            if(!visited[sr]) {
+                provinces++;
+                dfs(sr, isConnected, visited);
+            }
+        }
+        return provinces;
+    }
+    
+    private void dfs(int sr, int[][] mat, boolean[] visited) {
+        int n = mat.length;
+        visited[sr] = true;
+        for(int i=0; i<n; i++) {
+            if(mat[sr][i] == 1 && visited[i] == false) {
+                dfs(i, mat, visited);
+            }
+        }
+    }
+}
---
+++ DSA_Revision/src/com/wissen/graph/NumberOfIslands.java
@@ -0,0 +1,51 @@
+package com.wissen.graph;
+
+public class NumberOfIslands {
+	
+	//TC: O(m*n)
+    //SC: O(m*n)
+    public int numIslands(char[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        boolean[][] visited = new boolean[row][col];
+        int islands = 0;
+        
+        for(int i=0; i<row; i++) {
+            for(int j=0; j<col; j++) {
+                if(grid[i][j] == '1' && visited[i][j] == false) {
+                    islands++;
+                    dfs(i, j, grid, visited);
+                }
+            }
+        }
+        return islands;
+    }
+    
+    private void dfs(int i, int j, char[][] grid, boolean[][] visited) {
+        //base case
+        if(isValid(i, j, grid) == false) {
+            return;
+        }
+        
+        if(grid[i][j] == '0' || visited[i][j] == true) {
+            return;
+        }
+        //mark current grid as visited.
+        visited[i][j] = true;
+        //move into four directions horizontally & vertically
+        dfs(i-1, j, grid, visited); //up
+        dfs(i, j+1, grid, visited); //right
+        dfs(i+1, j, grid, visited); //down
+        dfs(i, j-1, grid, visited); //left
+    }
+    
+    private boolean isValid(int i, int j, char[][] grid) {
+        return i>=0 && j>=0 && i < grid.length && j <grid[0].length;
+    }
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
