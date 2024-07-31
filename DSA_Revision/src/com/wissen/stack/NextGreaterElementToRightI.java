4cbdce445771abb3de7857902d79bd17ad589620
dhakadrohan98 <rohandhakad20@gmail.com>
2024-07-31 16:13:26 +0530

    Designed Min stack & Next greater element to right problem is solved

---
+++ DSA_Revision/src/com/wissen/stack/NextGreaterElementToRightI.java
@@ -0,0 +1,38 @@
+package com.wissen.stack;
+
+import java.util.*;
+public class NextGreaterElementToRightI {
+	
+	public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        long[] ans = new long[n];
+        Stack<Long> st = new Stack<>();
+        
+        for(int i = n-1; i >= 0; i--) {
+            //if stack is empty
+            if(st.isEmpty()) {
+                ans[i] = -1;
+            } else if(!st.isEmpty() && st.peek() > arr[i]) {
+                ans[i] = st.peek();
+            } else if(!st.isEmpty() && st.peek() <= arr[i]){
+                while(!st.isEmpty() && st.peek() <= arr[i]) {
+                    st.pop();
+                }
+                //if stack is not empty means we found greater elemenet on the top of stack
+                if(!st.isEmpty()) {
+                    ans[i] = st.peek();
+                } else { //stack is empty, greater element is not present there
+                    ans[i] = -1;
+                }
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    } 
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
