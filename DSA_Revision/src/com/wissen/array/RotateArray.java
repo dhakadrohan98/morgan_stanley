423b508d1c81fe428574c7d7f77c12862fdbc629
dhakadrohan98 <rohandhakad20@gmail.com>
2024-08-06 17:18:44 +0530

    Top k frequent element, Rotate array by k and maximum product of three numbers are solved

---
+++ DSA_Revision/src/com/wissen/array/RotateArray.java
@@ -0,0 +1,38 @@
+package com.wissen.array;
+
+import java.util.Arrays;
+
+public class RotateArray {
+
+	//Keep dry run sample test cases to understand this algorithm completely
+	//TC: O(2*n)
+	//SC: O(1)
+	public static void rotate(int[] nums, int k) {
+		int n = nums.length;
+		k = k % n; // To handle cases where k > n
+		reverse(nums, 0, n - 1);
+		reverse(nums, 0, k - 1);
+		reverse(nums, k, n - 1);
+	}
+
+	private static void reverse(int[] nums, int start, int end) {
+		while (start < end) {
+			int temp = nums[start];
+			nums[start] = nums[end];
+			nums[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] nums = {1,2,3,4,5,6,7};
+		int k = 3;
+		int k1 = 9;
+		
+		rotate(nums, k1);
+		System.out.println(Arrays.toString(nums));
+
+	}
+
+}
