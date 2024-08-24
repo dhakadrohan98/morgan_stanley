803d45579906633cb4f357be40fd5ff46d29f656
dhakadrohan98 <rohandhakad20@gmail.com>
2024-08-24 23:42:53 +0530

    Solving Find the closest plindrome problem, 104 out of 217 test cases have been passed

--- DSA_Revision/src/com/wissen/array/MaximumOfThreeProducts.java
+++ DSA_Revision/src/com/wissen/array/MaximumOfThreeProducts.java
@@ -1,53 +1,50 @@
 package com.wissen.array;
 
 public class MaximumOfThreeProducts {
-	
-	//TC: O(n)
-    //SC: O(1)
-    public int maximumProduct(int[] nums) {
-        int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
-        int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
-        int n = nums.length;
-        int i=0;
-        
-        while(i < n) {
-            //keep track of max1, max2, max3
-            if(max1 < nums[i]) {
-                max3 = max2;
-                max2 = max1;
-                max1 = nums[i];
-            }
-            else if(max2 < nums[i]) {
-                max3 = max2;
-                max2 = nums[i];
-            }
-            else if(max3 < nums[i]) {
-                max3 = nums[i];
-            }
-            
-            //keep track of min1, min2, min3
-            if(min1 > nums[i]) {
-                min2 = min1;
-                min1 = nums[i];
-            }
-            else if(min2 > nums[i]) {
-                min2 = nums[i];
-            }
-                
-            i++;
-        }
-       
-        //Now do the calculation for max & min values to find the greater product
-        int firstProd = max3 * max2 * max1;
-        int secondProd = min1 * min2 * max1;
-        
-        if(firstProd > secondProd) {
-            return firstProd;
-        } else {
-            return secondProd;
-        }
-    }
-    
+
+	// TC: O(n)
+	// SC: O(1)
+	public int maximumProduct(int[] nums) {
+		int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE;
+		int min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
+		int n = nums.length;
+		int i = 0;
+
+		while (i < n) {
+			// keep track of max1, max2, max3
+			if (max1 < nums[i]) {
+				max3 = max2;
+				max2 = max1;
+				max1 = nums[i];
+			} else if (max2 < nums[i]) {
+				max3 = max2;
+				max2 = nums[i];
+			} else if (max3 < nums[i]) {
+				max3 = nums[i];
+			}
+
+			// keep track of min1, min2, min3
+			if (min1 > nums[i]) {
+				min2 = min1;
+				min1 = nums[i];
+			} else if (min2 > nums[i]) {
+				min2 = nums[i];
+			}
+
+			i++;
+		}
+
+		// Now do the calculation for max & min values to find the greater product
+		int firstProd = max3 * max2 * max1;
+		int secondProd = min1 * min2 * max1;
+
+		if (firstProd > secondProd) {
+			return firstProd;
+		} else {
+			return secondProd;
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
