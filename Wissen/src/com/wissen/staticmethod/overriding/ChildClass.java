59f2d2fa65d0d19b1a2e02972189a01c00d4aa45
dhakadrohan98 <rohandhakad20@gmail.com>
2024-10-06 14:08:29 +0530

    Make sum divisble by P leetcode problem is solved

--- Wissen/src/com/wissen/staticmethod/overriding/ChildClass.java
+++ Wissen/src/com/wissen/staticmethod/overriding/ChildClass.java
@@ -11,8 +11,8 @@ public class ChildClass extends StaticMethodOverriding {
 	
 	public static void main(String[] args) {
 		//creating object of parent(StaticMethodOverriding) class
-		StaticMethodOverriding childObj = new ChildClass();
-		System.out.println(childObj.sum(2, 3)); //5
+		StaticMethodOverriding parentClassObj = new ChildClass();
+		System.out.println(parentClassObj.sum(2, 3)); //5
 		
 		//creating object of child class
 		ChildClass childObj2 = new ChildClass();
