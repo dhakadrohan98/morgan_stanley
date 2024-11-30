d535776d44dda182cc7945647e6a55e31eea4a08
dhakadrohan98 <rohandhakad20@gmail.com>
2024-11-30 20:19:53 +0530

    Minimum LCM (ICPC, NERC region 2022-2023) problem is solved on codeforces

--- Codeforces_Problems/src/com/codeforces/numbertheory/MinimumLCM.java
+++ Codeforces_Problems/src/com/codeforces/numbertheory/MinimumLCM.java
@@ -2,40 +2,41 @@ package com.codeforces.numbertheory;
 
 import java.util.Scanner;
 
+//TC: O(sqrt(n) for each test case)
 public class MinimumLCM {
 
-	public static int hcf(int num1, int num2) {
+	public static long hcf(long num1, long num2) {
 		while (num1 % num2 != 0) {
-			int rem = num1 % num2;
+			long rem = num1 % num2;
 			num1 = num2;
 			num2 = rem;
 		}
 		return num2;
 	}
 
-	public static int lcm(int num1, int num2) {
-		int lcm = (num1 * num2) / hcf(num1, num2);
+	public static long lcm(long num1, long num2) {
+		long lcm = (num1 * num2) / hcf(num1, num2);
 		return lcm;
 	}
 
 	private static void solve(Scanner sc) {
-		int n = sc.nextInt();
+		long n = sc.nextLong();
 		//initialize LCM with largest integer value
-		int LCM = (int) (Math.pow(2, 11));
-		int first_no = 1;
-		int second_no = n - 1;
+		long LCM = (long) (9e18);
+		long first_no = 1;
+		long second_no = n - 1;
 		//find all divisors & lcm (of divisor pair) for n
-		for(int d = 2; (d*d) <= n; d++) {
+		for(long d = 2; (d*d) <= n; d++) {
 			//first case (b % a = 0)
 				//first divisor -> a'
 				// n = 12
 				// d = 2;
 				//a = 2;
 			if(n % d == 0) {
-				int a = d;
-				int b =  n - d;
+				long a = d;
+				long b =  n - d;
 				//find lcm for current pairs
-				int lcm_val = (a*b) / hcf(a, b);
+				long lcm_val = (a*b) / hcf(a, b);
 				if(lcm_val < LCM) {
 					LCM = lcm_val;
 					first_no = a;
@@ -47,8 +48,8 @@ public class MinimumLCM {
 					// d = 2;
 					//a1 = 12 / 2;
 					//a1 = 6;
-				int a1 = n / d;
-				int b1 = n - a1;
+				long a1 = n / d;
+				long b1 = n - a1;
 				//find lcm for current pairs
 				lcm_val = (a1*b1) / hcf(a1, b1);
 				if(lcm_val < LCM) {
